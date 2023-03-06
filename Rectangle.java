6ecabaa9a51e7b24e184225282fde2fcc04859d2
MaxHorin200319 <124078832+MaxHorin200319@users.noreply.github.com>
2023-03-06 16:35:34 +0200

    Add files via upload

---
+++ Rectangle.java
@@ -0,0 +1,20 @@
+package Homework2;
+
+public class Rectangle {
+    double side1,side2;
+    public Rectangle(){}
+    public double areaCalculator(double num1,double num2){
+        side1 = (num1 * num2);
+        return side1;
+    }
+    public double perimeterCalculator(double num1,double num2){
+        side2 = (num1 + num2 + num1 + num2);
+        return side2;
+    }
+
+    public void viewResult(){
+        System.out.println("Площа прямокутника = "+side1+ "см");
+        System.out.println("Периметр прямокутника = "+side2+ "см");
+    }
+}
+
